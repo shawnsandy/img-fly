fbe60a7adab6f26cb31a222239195a4b64572ed0
Shawn Sandy <shawnsandy04@gmail.com>
2017-11-19 10:40:42 -0500

    Fix : import missing classes

--- src/Controllers/ImgController.php
+++ src/Controllers/ImgController.php
@@ -3,13 +3,18 @@
 namespace ShawnSandy\ImgFly\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use League\Glide\Responses\LaravelResponseFactory;
+use League\Glide\ServerFactory;
 
 
 class ImgController extends Controller
 {
 
+
 	public function __invoke($dir, $photo)
-	{
+			{
 		$server = ServerFactory::create([
 			'source' => "./$dir/",
 			'cache' =>  "$dir/",
