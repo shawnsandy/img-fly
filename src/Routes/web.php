cb19d2933bd8f2c18a70168c8b28f10825da0fe6
Shawn Sandy <shawnsandy04@gmail.com>
2017-11-22 17:28:39 -0500

    reset default route

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,6 +4,6 @@ Route::group(['prefix' => 'imgfly'], function(){
 
     Route::get('/public/{dir}/{path}', '\ShawnSandy\ImgFly\Controllers\ImgController')->where('path', '.+');
 
-    Route::get('/img/{dir}/{path}', '\ShawnSandy\ImgFly\Controllers\ImgflyController')->where('path', '.+');
+    Route::get('/{dir}/{path}', '\ShawnSandy\ImgFly\Controllers\ImgflyController')->where('path', '.+');
 
 });
