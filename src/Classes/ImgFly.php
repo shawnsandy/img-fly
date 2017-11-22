7ee79ea9e16ef340b298f72c60a56a7d6585caf6
Shawn Sandy <shawnsandy04@gmail.com>
2017-11-21 19:18:50 -0500

    Feature : resize image based on predefined presets - imgPresetMethod()

--- src/Classes/ImgFly.php
+++ src/Classes/ImgFly.php
@@ -7,42 +7,62 @@ use Illuminate\Support\Facades\Facade;
 class ImgFly
 {
 
-	public function routes() {
-		include __DIR__.'../../Routes/web.php';
-	}
+    public function routes()
+    {
+        include __DIR__ . '../../Routes/web.php';
+    }
 
-	/**
-	 * Display images in storage/app
-	 *
-	 * @deprecated v0.1beta use img()
-	 * @param [string] $image_str
-	 * @return void
-	 */
-	public function imgFly($image_str) {
-		return url('imgfly/images/'.$image_str);
-	}
+    /**
+     * Display images in storage/app
+     *
+     * @deprecated v0.1beta use img()
+     * @param [string] $image_str
+     * @return void
+     */
+    public function imgFly($image_str)
+    {
+        return url('imgfly/images/' . $image_str);
+    }
 
-	/**
-	 * Display images in storage/app
-	 *
-	 *
-	 * @param [string] $image_str
-	 * @return void
-	 */
-	public function img($image_str) {
-		return url('imgfly/images/'.$image_str);
-	}
+    /**
+     * Display images in storage/app
+     *
+     *
+     * @param [string] $image_str
+     * @return void
+     */
+    public function img($image_str)
+    {
+        return url('imgfly/images/' . $image_str);
+    }
 
 
-	/**
-	 * Display images from public/img
-	 *
-	 * @param [string] $image_str
-	 * @param string $dir
-	 * @return void
-	 */
-	public function imgPublic($image_str, $dir = 'img'){
-		return url("imgfly/public/$dir/$image_str");
-	}
+    /**
+     * Display images from public/img
+     *
+     * @param [string] $image_str
+     * @param string $dir
+     * @return void
+     */
+    public function imgPublic($image_str, $dir = 'img')
+    {
+        return url("imgfly/public/$dir/$image_str");
+    }
+
+    /**
+     * @param string $preset
+     * @param null|string $storageMethod
+     * @return mixed
+     */
+    public function imgPreset($img, $preset = 'small', $storageMethod = 'img')
+    {
+
+
+        $parameters = config("imgfly.{$preset}");
+
+      return  call_user_func([$this, $storageMethod], $img.$parameters);
+
+    }
+
 
 }
