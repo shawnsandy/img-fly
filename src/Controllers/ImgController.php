5c561a7fb59ddd0241c6ee783eab9bdffd79fe48
Ayberk <posta@ayberkcal.com>
2020-11-27 22:13:00 +0300

    update composer, add not found image handle via config

--- src/Controllers/ImgController.php
+++ src/Controllers/ImgController.php
@@ -11,6 +11,13 @@ class ImgController extends Controller
 {
     public function __invoke($dir, $photo)
     {
+//        if (!file_exists($dir . '/' . $photo)) {
+//            if (config("logNotFoundImage"))
+//                \Log::warning("Image Not Found - Path:'$dir . '/' . $photo'");
+//            if (config("notFoundImagePath") != null)
+//                $dir = config("notFoundImagePath");
+//        }
+
         $server = ServerFactory::create([
             'response' => new LaravelResponseFactory(app('request')),
             'source' => "./$dir/",
@@ -18,7 +25,14 @@ class ImgController extends Controller
             'source_path_prefix' => '/',
             'cache_path_prefix' => '/.cache',
             'base_url' => '/public/'
-            ]);
+        ]);
+
+        if (!$server->sourceFileExists($photo)) {
+            if (config("imgfly.logNotFoundImage"))
+                \Log::warning("Image Not Found - Path:' " . $dir . '/' . $photo . "'");
+            if (config("imgfly.notFoundImagePath") != null)
+                return $server->outputImage(config("imgfly.notFoundImagePath"), request()->all());
+        }
 
         return $server->outputImage($photo, request()->all());
     }
